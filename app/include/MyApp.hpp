f587b35e2a9f4ad69185fd1370582d6caef37c33
jananenic <jana.nenic@gmail.com>
2026-08-31 12:42:07 +0200

    'Create empty MyApp and app_setup'

---
+++ app/include/MyApp.hpp
@@ -0,0 +1,18 @@
+//
+// Created by matfrg on 8/30/26.
+//
+
+#ifndef MYAPP_HPP
+#define MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+
+class MyApp : public engine::core::App{
+    void app_setup() override;
+
+};
+
+} // app
+
+#endif //MYAPP_HPP
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,13 @@
+//
+// Created by matfrg on 8/30/26.
+//
+
+#include <MyApp.hpp>
+#include <spdlog/spdlog.h>
+
+namespace app {
+
+void MyApp::app_setup() {
+    spdlog::info("App setup completed!");
+}
+} // app
