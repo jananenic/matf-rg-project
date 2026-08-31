be0f27c066271f52a07cbd7d35efab60740fca89
jananenic <jana.nenic@gmail.com>
2026-08-31 13:14:54 +0200

    Create base for MainController

---
+++ app/src/MainController.cpp
@@ -0,0 +1,13 @@
+//
+// Created by matfrg on 8/31/26.
+//
+
+#include "../include/MainController.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+    void MainController::initialize() {
+        spdlog::info("MainController initialized");
+    }
+} // app
--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -2,6 +2,7 @@
 // Created by matfrg on 8/30/26.
 //
 
+#include <MainController.hpp>
 #include <MyApp.hpp>
 #include <spdlog/spdlog.h>
 
@@ -9,5 +10,8 @@ namespace app {
 
 void MyApp::app_setup() {
     spdlog::info("App setup completed!");
+    auto main_controller = register_controller<app::MainController>();
+    main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+
 }
 } // app
