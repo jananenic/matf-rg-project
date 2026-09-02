09d6cdc40ea029402d7eb3e967ac99190d373a85
jananenic <jana.nenic@gmail.com>
2026-09-02 14:10:57 +0200

    Draw wood swing.

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,12 +4,16 @@
 
 #include "../include/MainController.hpp"
 
+#include <engine/graphics/GraphicsController.hpp>
+#include <engine/graphics/OpenGL.hpp>
 #include <engine/platform/PlatformController.hpp>
+#include <engine/resources/ResourcesController.hpp>
 #include <spdlog/spdlog.h>
 
 namespace app {
     void MainController::initialize() {
-        spdlog::info("MainController initialized");
+        engine::graphics::OpenGL::enable_depth_testing();
+        //spdlog::info("MainController initialized");
     }
 
     bool MainController::loop() {
@@ -18,5 +22,38 @@ namespace app {
             return false;
         }
         return true;
+    }
+
+    void MainController::draw_woodswing() {
+        //model
+        auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        engine::resources::Model* woodswing = resources->model("woodswing");
+
+        //shader
+        engine::resources::Shader* shader = resources->shader("basic");
+
+        shader->use();
+        shader->set_mat4("projection", graphics->projection_matrix());
+        shader->set_mat4("view", graphics->camera()->view_matrix());
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        model = glm::scale(model, glm::vec3(0.3f));
+        shader->set_mat4("model", model);
+        woodswing->draw(shader);
+    }
+
+    void MainController::begin_draw() {
+        engine::graphics::OpenGL::clear_buffers();
+
+    }
+
+    void MainController::draw() {
+        draw_woodswing();
+    }
+
+    void MainController::end_draw() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        platform->swap_buffers();
     }
 } // app
